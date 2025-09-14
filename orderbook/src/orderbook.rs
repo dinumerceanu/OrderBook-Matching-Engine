22cfcab0824ef7c63b37e2e1d525d29d05d123a7
dinumerceanu <dinu.eu@yahoo.com>
2025-09-14 13:35:17 +0300

    small adjustemnts to the matching engine

--- orderbook/src/orderbook.rs
+++ orderbook/src/orderbook.rs
@@ -46,25 +46,33 @@ impl OrderBook {
         }
     }
 
-    fn get_best_bid(&mut self) -> Option<(usize, &mut VecDeque<LimitOrder>)> {
-        if let Some((&price, _)) = self.bids.last_key_value() {
-            if let Some(queue) = self.bids.get_mut(&price) {
-                Some((price, queue))
-            } else {
-                None
+    fn notify(tx: mpsc::Sender<String>, msg: String) {
+        tokio::spawn(async move {
+            if let Err(e) = tx.send(msg).await {
+                eprintln!("Error writing to channel: {e}");
             }
+        });
+    }
+
+    fn send_price(tx: mpsc::UnboundedSender<usize>, price: usize) {
+        tokio::spawn(async move {
+            if let Err(e) = tx.send(price) {
+                eprintln!("Error writing price on channel: {e}");
+            }
+        });
+    }
+
+    fn get_best_bid(&mut self) -> Option<usize> {
+        if let Some((&price, _)) = self.bids.last_key_value() {
+            Some(price)
         } else {
             None
         }
     }
 
-    fn get_best_ask(&mut self) -> Option<(usize, &mut VecDeque<LimitOrder>)> {
+    fn get_best_ask(&mut self) -> Option<usize> {
         if let Some((&price, _)) = self.asks.first_key_value() {
-            if let Some(queue) = self.asks.get_mut(&price) {
-                Some((price, queue))
-            } else {
-                None
-            }
+            Some(price)
         } else {
             None
         }
@@ -77,14 +85,19 @@ impl OrderBook {
             if available_market_order_size == 0 {
                 break;
             }
-            
+
             let best_price = if market_order.side() == MarketSide::Ask {
                 self.get_best_bid()
             } else {
                 self.get_best_ask()
             };
 
-            if let Some((price, orders_queue)) = best_price {
+            if let Some(price) = best_price {
+                let orders_queue = if market_order.side() == MarketSide::Ask {
+                    self.bids.get_mut(&price).unwrap()
+                } else {
+                    self.asks.get_mut(&price).unwrap()
+                };
                 loop {
                     let available_market_order_size = market_order.size() - market_order.fill_size();
                     if available_market_order_size <= 0 {
@@ -94,79 +107,70 @@ impl OrderBook {
                         let available_limit_order_size = limit_order.size() - limit_order.fill_size();
 
                         if available_limit_order_size < available_market_order_size {
-                            let msg_full_fill = format!("Order filled!");
+                            let msg_full_fill = format!("Order filled [{}/{}]", available_limit_order_size, limit_order.size());
                             let msg_partial_fill = format!("Order filled [{}/{}] at {}", available_limit_order_size, market_order.size(), limit_order.price());
+                            
                             let limit_client_tx = limit_order.client().tx();
                             let market_client_tx = market_order.client().tx();
+
                             market_order.set_fill_size(market_order.fill_size() + available_limit_order_size);
 
-                            tokio::spawn(async move {
-                                if let Err(e) = limit_client_tx.send(msg_full_fill).await {
-                                    eprintln!("Error writing to channel: {e}");
-                                }
-                            });
-                            tokio::spawn(async move {
-                                if let Err(e) = market_client_tx.send(msg_partial_fill).await {
-                                    eprintln!("Error writing to channel: {e}");
-                                }
-                            });
-                            if let Err(e) = tx_price.send(limit_order.price()) {
-                                eprintln!("Error writing price on channel: {e}");
-                            }
+                            Self::notify(limit_client_tx, msg_full_fill);
+                            Self::notify(market_client_tx, msg_partial_fill);
+                            Self::send_price(tx_price.clone(), limit_order.price());
+
                         } else if available_limit_order_size == available_market_order_size {
                             let msg_market_client = format!("Order filled [{}/{}] at {}", available_market_order_size, market_order.size(), limit_order.price());
+                            let msg_limit_client = format!("Order filled [{}/{}]", available_limit_order_size, limit_order.size());
                             let limit_client_tx = limit_order.client().tx();
                             let market_client_tx = market_order.client().tx();
-                            market_order.set_fill_size(market_order.fill_size() + available_market_order_size);
                             let price = limit_order.price();
-                            tokio::spawn(async move {
-                                let msg = format!("Order filled [{}/{}]", available_limit_order_size, limit_order.size());
-                                if let Err(e) = limit_client_tx.send(msg).await {
-                                    eprintln!("Error writing to channel: {e}");
-                                }
-                            });
-                            tokio::spawn(async move {
-                                if let Err(e) = market_client_tx.send(msg_market_client).await {
-                                    eprintln!("Error writing to channel: {e}");
-                                }
-                            });
-                            if let Err(e) = tx_price.send(price) {
-                                eprintln!("Error writing price on channel: {e}");
-                            }
+
+                            market_order.set_fill_size(market_order.fill_size() + available_market_order_size);
+                            
+                            Self::notify(limit_client_tx, msg_limit_client);
+                            Self::notify(market_client_tx, msg_market_client);
+                            Self::send_price(tx_price.clone(), price);
+
                             break;
                         } else {
                             let msg_full_fill = format!("Order filled [{}/{}] at {}", available_market_order_size, market_order.size(), limit_order.price());
                             let msg_partial_fill = format!("Order filled [{}/{}]", available_market_order_size, limit_order.size());
+
                             let limit_client_tx = limit_order.client().tx();
                             let market_client_tx = market_order.client().tx();
+
+                            let price= limit_order.price();
+
                             limit_order.set_fill_size(limit_order.fill_size() + available_market_order_size);
                             market_order.set_fill_size(market_order.fill_size() + available_market_order_size);
-                            let price= limit_order.price();
+
                             orders_queue.push_front(limit_order);
-                            tokio::spawn(async move {
-                                if let Err(e) = limit_client_tx.send(msg_partial_fill).await {
-                                    eprintln!("Error writing to channel: {e}");
-                                }
-                            });
-                            tokio::spawn(async move {
-                                if let Err(e) = market_client_tx.send(msg_full_fill).await {
-                                    eprintln!("Error writing to channel: {e}");
-                                }
-                            });
-                            if let Err(e) = tx_price.send(price) {
-                                eprintln!("Error writing price on channel: {e}");
-                            }
+
+                            Self::notify(limit_client_tx, msg_partial_fill);
+                            Self::notify(market_client_tx, msg_full_fill);
+                            Self::send_price(tx_price.clone(), price);
+
                             break;
                         }
                     } else {
                         if market_order.side() == MarketSide::Ask {
                             println!("No bids at {price}");
-                            self.bids.remove_entry(&price);
                         } else {
                             println!("No asks at {price}");
-                            self.asks.remove_entry(&price);
                         }
                         break;
+                    }
+                }
+                let empty = orders_queue.is_empty();
+                if empty {
+                    match market_order.side() {
+                        MarketSide::Ask => {
+                            self.bids.remove(&price);
+                        },
+                        MarketSide::Bid => {
+                            self.asks.remove(&price);
+                        }
                     }
                 }
             } else {
